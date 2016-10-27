5db04b00e36574181fea165d8887fe860141cbae
jianglingfeng <18575528748@163.com>
2016-10-27 16:20:43 +0800

    创建Cat类

---
+++ weixin/weixin/Cat.h
@@ -0,0 +1,13 @@
+//
+//  Cat.h
+//  weixin
+//
+//  Created by jianglingfeng on 16/10/27.
+//  Copyright © 2016年 Daoyitong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cat : NSObject
+
+@end
